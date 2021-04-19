d7fc7bf8f68e5afec71a2cf019ea6a5209bfd72e
octavio villavicencio cruz <octaviovcruz@hotmail.com>
2021-04-18 22:21:48 -0500

    Maquetación y estilos del flujo de pago

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styles/components/Success.css'
 
 const Success = () => {
     return (
-        <div>
-            Success
+        <div className="Success">
+            <div className="Success-content">
+                <h2>Octavio, Gracias por tu compra</h2>
+                <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+                <div className="Success-map">
+                    Googel Maps
+                </div>
+            </div>
         </div>
     )
 }
